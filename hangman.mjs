b544a176fc3a0bc785336456989c4d8253e7af46
Brody Blair <brocbla@umich.edu>
2023-03-21 17:17:51 -0500

    first commit

--- hangman.mjs
+++ hangman.mjs
@@ -1,7 +1,7 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
-const WORD_BANK = ["paradiddle", "thiccboi", "oreos", "quests", "spackle", "smashburger"];
+const WORD_BANK = ["paradiddle", "thiccboi", "oreos", "quests", "spackle", "smashburger", "dragonfamilyvalues", "pretzels", "newlocks", "youtube", "saladbowl", "root", "batteries", "butterfly", "googlehomeisalwayslistening", "eldorado", "familydollar", "mug", "toast", "listenhereyoulittle", "bowling", "baileys", "sushi", "florida"];
 const MASK_CHAR = "_ ";
 const randomIndex = Math.floor(Math.random() * WORD_BANK.length);
 let randomWord = WORD_BANK[randomIndex];
@@ -9,6 +9,7 @@ const correctGuesses = [];
 let guessCount = 0
 const MAX_GUESSES = 6
 let maskedWord = buildMaskedWord(randomWord)
+const LETTERS_GUESSED = [];
 
 function buildMaskedWord(word) {
     let builder = "";
@@ -17,9 +18,6 @@ function buildMaskedWord(word) {
     }
     return builder;
 }
-
-
-
 
 const regularExpression = /[a-zA-Z]+$/
 function validateLetter(userGuess) {
@@ -35,18 +33,25 @@ function updateState(guess) {
     if (isCorrectGuess(guess)) {
         correctGuesses.push(guess);
         maskedWord = replaceMaskedChar(guess);
+    } else if (LETTERS_GUESSED.includes(guess)) {
+        console.log(`You already guessed "${guess}"! Try something different!`)
     } else {
         guessCount++;
+        LETTERS_GUESSED.unshift(guess);
     }
+
     console.log(`Guesses Remaing: ${guessCount}/${MAX_GUESSES}`);
+    console.log(`Guessed letters: ${LETTERS_GUESSED}`);
     console.log(maskedWord);
+
 }
+
 
 
 function replaceMaskedChar() {
     let builder = "";
     for (let i = 0; i < randomWord.length; i++) {
-        let character = randomWord[i]
+        let character = randomWord[i].toLowerCase()
         if (correctGuesses.includes(character)) {
             builder = builder.concat(character, " ");
         } else {
@@ -80,18 +85,19 @@ async function gameLoop() {
     const rl = readline.createInterface({ input, output });
     while (!isDone) {
         const userGuess = await rl.question('Guess a letter!...if you dare ');
-        const trimmedGuess = userGuess.trim()
+        const trimmedGuess = userGuess.trim().toLowerCase()
         const isValid = validateLetter(trimmedGuess)
-        console.log(`Let's see if ${userGuess} is in the word`);
+        console.log(`Let's see if "${userGuess}" is in the word`);
         if (isValid) {
             updateState(trimmedGuess)
-        if (hasLost()) {
-            isDone = true
-            console.log("Loser!")
-        } else if (hasWon()) {
-            isDone = true
-            console.log("winner winner looking thinner!");
-        }
+            if (hasLost()) {
+                isDone = true
+                console.log("Loser!")
+                console.log(`The word (or sentence) was "${randomWord}!"`)
+            } else if (hasWon()) {
+                isDone = true
+                console.log("winner winner looking thinner!");
+            }
         } else {
             console.log('Please guess A letter (the a is capitalized to emphasize sass')
         }
